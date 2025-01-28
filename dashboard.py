afc88123e7b3c1b4bebfd7758552a72851f39652
NamaPenggunaBaru <dsctrilogi@gmail.com>
2025-01-29 01:39:43 +0700

    menambah file csv

--- dashboard.py
+++ dashboard.py
@@ -1,9 +1,3 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import streamlit as st
-
 # Load your dataset (replace 'all_data.csv' with your actual file path)
 df_items = pd.read_csv("order_items_dataset.csv")
 df_reviews = pd.read_csv("order_reviews_dataset.csv")
